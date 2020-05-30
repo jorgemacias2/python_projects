bf9aacaa8fa905826295de30181a689ab68c398a
jorgemacias2 <35404558+jorgemacias2@users.noreply.github.com>
2020-05-29 19:25:33 -0500

    Full project
    Full project

--- planetary-api/app.py
+++ planetary-api/app.py
@@ -157,6 +157,60 @@ def planet_details(planet_id: int):
         return jsonify(message="That planet does no exist"), 404
 
 
+@app.route("/add_planet", methods=["POST"])
+@jwt_required
+def add_planet():
+    planet_name = request.form["planet_name"]
+    test = Planet.query.filter_by(planet_name=planet_name).first()
+    if test:
+        return jsonify("There is already a planet by that name"), 409
+    else:
+        planet_type = request.form["planet_type"]
+        home_star = request.form["home_star"]
+        mass = float(request.form["mass"])
+        radius = float(request.form["radius"])
+        distance = float(request.form["distance"])
+
+        new_planet = Planet(planet_name=planet_name,
+                            planet_type=planet_type,
+                            home_star=home_star,
+                            mass=mass,
+                            radius=radius,
+                            distance=distance)
+
+    db.session.add(new_planet)
+    db.session.commit()
+    return jsonify(message="You added a planet"),201
+
+@app.route("/update_planet", methods=["PUT"])
+@jwt_required
+def update_planet():
+    planet_id = int(request.form["planet_id"])
+    planet = Planet.query.filter_by(planet_id=planet_id).first()
+    if planet:
+        planet.planet_name = request.form["planet_name"]
+        planet.planet_type = request.form["planet_type"]
+        planet.home_star = request.form["home_star"]
+        planet.mass = float(request.form["mass"])
+        planet.radius = float(request.form["radius"])
+        planet.distance = float(request.form["distance"])
+        db.session.commit()
+        return jsonify(message="You updated a planet"), 202
+    else:
+        return jsonify(message="That planet does not exist")
+
+@app.route("/remove_planet/<int:planet_id>", methods=["DELETE"])
+@jwt_required
+def remove_planet(planet_id: int):
+    planet = Planet.query.filter_by(planet_id=planet_id).first()
+    if planet:
+        db.session.delete(planet)
+        db.session.commit()
+        return jsonify(message="You deleted a planet"), 202
+    else:
+        return jsonify(message="That planet does not exist"), 404
+
+
 # database models
 class User(db.Model):
     __tablename__ = "users"
